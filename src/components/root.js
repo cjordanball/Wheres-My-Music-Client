245dbb053e7fba107431e5bea8ba0980ba416f0d
Jordan Ball <jordanball@WTA-MBP525.fios-router.home>
2019-03-04 22:57:12 -0500

    initial setup

---
+++ src/components/root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Root = () => (
+	<div>
+		<h3>Where&#39s My Music?</h3>
+	</div>
+);
+
+export default Root;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import styles from './styles/styles.less';
+import Root from './components/Root';
+import './assets/images/ballIcon.ico';
+
+ReactDOM.render(
+	<Root />, document.querySelector('#root')
+);
