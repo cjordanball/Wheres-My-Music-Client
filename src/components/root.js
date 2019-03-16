1defa62a2d2fd73052d118c8c0847b3d4c81415a
Jordan Ball <jordanball@WTA-MBP525.fios-router.home>
2019-03-15 22:38:44 -0400

    add graphql to client side

--- src/components/root.js
+++ src/components/root.js
@@ -1,9 +1,24 @@
 import React from 'react';
+// import {
+// 	Router,
+// 	Route, hashHistory
+// } from 'react-router';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import Home from './home';
+
+
+const client = new ApolloClient({
+	link: new HttpLink({ uri: 'http://localhost:3600/graphql' }),
+	cache: new InMemoryCache()
+});
 
 const Root = () => (
-	<div>
-		<h3>Where&apos;s My Music?</h3>
-	</div>
+	<ApolloProvider client={client}>
+		<Home />
+	</ApolloProvider>
 );
 
 export default Root;
--- webpack.config.js
+++ webpack.config.js
@@ -2,64 +2,64 @@ const HtmlWebPackPlugin = require ('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: {
-                            minimize: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [{
-                    loader: 'style-loader'
-                },
-                {
-                    loader: 'css-loader'
-                },
-                {
-                    loader: 'less-loader'
-                }]
-            },
-            {
-                test: /\.(png|svg|jpg|gif|ico)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: 'src/assets/images/[name].[ext]'
-                    }
-                }]
-            },
-            {
-                test: /\.ttf$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'src/assets/fonts/'
-                    }
-                }]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './index.html',
-            filename: './index.html',
-            favicon: 'src/assets/images/ballIcon.ico'
-        })
-    ]
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.less$/,
+				use: [{
+					loader: 'style-loader'
+				},
+				{
+					loader: 'css-loader'
+				},
+				{
+					loader: 'less-loader'
+				}]
+			},
+			{
+				test: /\.(png|svg|jpg|gif|ico)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: 'src/assets/images/[name].[ext]'
+					}
+				}]
+			},
+			{
+				test: /\.ttf$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'src/assets/fonts/'
+					}
+				}]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './index.html',
+			filename: './index.html',
+			favicon: 'src/assets/images/ballIcon.ico'
+		})
+	]
 };
