d13fc0e9fae4a52f97f4821e2da35d87a3501f03
Jordan Ball <jordanball@WTA-MBP525.fios-router.home>
2019-03-18 23:01:12 -0400

    create landing page component

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import fetchScores from '../queries/fetchScores';
+import LandingPage from './landingPage';
 
 
 const Home = (props) => {
@@ -8,6 +9,7 @@ const Home = (props) => {
 	return (
 		<div>
 			<h3>Where&apos;s My Music?</h3>
+			<LandingPage />
 		</div>
 	);
 };
